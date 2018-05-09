e0f3348051afb35eb7bf4b370d5634b5b7cce059
Rabban <Rabban@192.168.0.9>
2018-05-09 16:09:47 +0530

    Story# sample code modified

--- src/com/nt/Sample.java
+++ src/com/nt/Sample.java
@@ -3,8 +3,8 @@ package com.nt;
 public class Sample {
 
 	public static void main(String[] args) {
-		int x=10;
-		int y=20;
+		int x=40;
+		int y=60;
 		int z=x+y;
 		System.out.println("Sum ::"+z);
 	}
